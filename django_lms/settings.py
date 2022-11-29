e71f6dfb3c34c621fc7bebb22e9c19cb9cdebd9c
wez-ux <63924875+wez-ux@users.noreply.github.com>
2022-11-29 09:26:53 +0000

    first commit

--- django_lms/settings.py
+++ django_lms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j7xv%)66l8u0^8sxnj7()z$zll8&a2*_h2r84yeo9f2fidx8^7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['web-production-0f05.up.railway.app']
 
 
 # Application definition
